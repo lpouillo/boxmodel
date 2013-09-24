417d8b720ae0a4c27e9241fa872261b27f407f16
Laurent Pouilloux <laurent.pouilloux@inria.fr>
2013-09-24 02:44:09 +0200

    Add a method to plot the state of the boxes

--- BoxModel.py
+++ BoxModel.py
@@ -21,6 +21,7 @@ from scipy.integrate import odeint
 from execo_engine import Engine, ParamSweeper, sweep, slugify, logger
 import numpy as np
 import matplotlib.pyplot as plt 
+import pydot as P
 
 
 class BoxModel(Engine):
@@ -38,17 +39,18 @@ class BoxModel(Engine):
         """ Execute the engine and compute the results """
         
         Ratio = odeint(self.evol_ratio, self.initial_state(), self.time)
+        self.plot_state(self.Delta.keys(), Ratio[:,0], name = '_initial')
+        self.plot_state(self.Delta.keys(), Ratio[:,-1], name = '_final')
         Delta_final = ((Ratio/self.standard_IRMM)-1.0)*1000;
-        print Delta_final[:,]
-#        plt.xkcd()
-#        p2 = plt.plot(self.time, Delta_final[:,1], label="liver")
         
+        self.plot_state(self.Delta.keys(), Ratio)
+
         
+    def plot_evolution(self, Delta):
         i_box = 0
-        for box, value in self.Delta.iteritems():
-            print box, value
+        for box in self.Boxes:
             if box not in [ 'feces', 'urine' ]:
-                plt.plot(self.time/365., Delta_final[:,i_box], label=box)
+                plt.plot(self.time/365., Delta[:,i_box], label=box)
             i_box += 1
         
         
@@ -67,10 +69,11 @@ class BoxModel(Engine):
         
         
     def initial_state(self):
-        self.Mass = np.array( self.Mass.values() )
-        self.Flux = np.array( [ box.values() for box in self.Flux.values() ])
-        self.Partcoeff = np.array( [ box.values() for box in self.Partcoeff.values() ])
-        Ratio = np.array( [ (delta/1e3+1e0)*self.standard_IRMM for delta in self.Delta.values() ] )
+        self._Mass = np.array( self.Mass.values() )
+        self._Flux = np.array( [ box.values() for box in self.Flux.values() ])
+        self._Partcoeff = np.array( [ box.values() for box in self.Partcoeff.values() ])
+        
+        Ratio = np.array( [ (delta/1e3+1e0)*self.standard_IRMM for delta in self.Delta.values() ] )    
         return Ratio
         
         
@@ -84,6 +87,8 @@ class BoxModel(Engine):
         self.time = np.linspace(0, 13870.0, n_timestep)  # temps
          
         self.standard_IRMM = 0.0637 
+        self.Boxes = [ "diet", "plasma", "RBC", "liver", "urine", "feces", "menses" ]
+                      
         self.Delta = {
              "diet":     1.0e0, 
              "plasma":  1.51e0, 
@@ -131,7 +136,28 @@ class BoxModel(Engine):
             influx=0;
             for jj in range(ratio.size):
                 # le nouveau ratio est calcule a partir des flux entrants et sortants
-                outflux = outflux + self.Flux[ii][jj]/self.Mass[ii]*self.Partcoeff[ii][jj]*ratio[ii]
-                influx = influx + self.Flux[jj][ii]/self.Mass[ii]*self.Partcoeff[jj][ii]*ratio[jj]
+                outflux = outflux + self._Flux[ii][jj]/self._Mass[ii]*self._Partcoeff[ii][jj]*ratio[ii]
+                influx = influx + self._Flux[jj][ii]/self._Mass[ii]*self._Partcoeff[jj][ii]*ratio[jj]
             rationew[ii]= influx - outflux
         return rationew;
+    
+    def plot_state(self, boxes, ratios, name = ''):
+        colors = {"diet": "#BBFFB5", "plasma": "#FFC66D", "RBC": "#BA0400", 
+                "liver": "#93BB8F", "urine": "#FBFF93", "feces": "#BF9285", "menses": "#FF0600"}
+        shapes = {"diet": "rectangle", "plasma": "ellipse", "RBC": "ellipse", 
+                "liver": "ellipse", "urine": "rectangle", "feces": "rectangle", "menses": "rectangle"}
+        graph = P.Dot(graph_type='digraph', fontname="Verdana", ratio = "1")
+        i_box = 0
+        for box in boxes:
+            node_box = P.Node(box, style="filled", label = box+'\n '+str(ratios[i_box]),
+                fillcolor=colors[box], shape = shapes[box])
+            i_box += 1
+            graph.add_node(node_box)
+            
+        for box_from, boxes_to in self.Flux.iteritems():
+            for box_to, flux in boxes_to.iteritems():
+                if flux > 0:
+                    edge = P.Edge(box_from, box_to,  label = flux)
+                    graph.add_edge(edge)
+        graph.write_png('state'+name+'.png')
+        
