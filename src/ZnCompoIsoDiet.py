036f849d66d0c2572e9b9bf594b960d176ee8ded
Laurent Pouilloux <lolo@pouilloux.org>
2015-01-28 17:11:45 +0100

    réécriture de l'engine en décomposant les méthodes

--- src/ZnCompoIsoDiet.py
+++ src/ZnCompoIsoDiet.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from IsotopicBoxModel import IsotopicBoxModel, sweep, ParamSweeper, slugify, \
-    logger, path, mkdir, linspace
+    logger, path, mkdir, linspace, style
+from numpy import arange
 
 
 class ZnCompoIsoDiet(IsotopicBoxModel):
@@ -11,15 +12,18 @@ class ZnCompoIsoDiet(IsotopicBoxModel):
     def __init__(self):
         """Set delta_name"""
         super(ZnCompoIsoDiet, self).__init__()
-        self.parameters()
         self.delta_name = r"$\delta^{66}Zn$"
 
     def run(self):
         """ Execute the engine and compute the results """
-        parameters = {'delta_diet': range(-0.1, 1.0),
-                      'coeff_DP': range(0.9996, 1.0004)}
+        parameters = {'delta_diet': arange(-0.1, 1.5, 0.5),
+                      'coeff_DP': arange(0.9995, 1.0005, 0.0003),
+                      'flux_DP': range(8, 17),
+                      'flux_PB': arange(0.01, 0.1, 0.03)}
         sweeps = sweep(parameters)
         sweeper = ParamSweeper(path.join(self.result_dir, "sweeps"), sweeps)
+        logger.info('Engine will treat %s models',
+                    style.emph(len(sweeper.get_remaining())))
 
         while len(sweeper.get_remaining()) > 0:
             comb = sweeper.get_next()
@@ -28,7 +32,9 @@ class ZnCompoIsoDiet(IsotopicBoxModel):
                 mkdir(comb_dir)
             except:
                 pass
-            self.set_flux(comb['flux_diet'], comb['flux_bone'])
+            self.set_boxes(comb['delta_diet'])
+            self.set_flux(comb['flux_DP'], comb['flux_PB'])
+            self.set_partcoeff(comb['coeff_DP'])
             Delta = []
             Delta = self.initial_state(outdir=comb_dir)
             Delta = self.compute_evolution(Delta, outdir=comb_dir)
@@ -39,26 +45,23 @@ class ZnCompoIsoDiet(IsotopicBoxModel):
         logger.info('All combinations have been done, result can be found in '
                     + self.result_dir)
 
-    def set_flux(self, flux_diet, flux_bone):
-        k = 0.33
-        t = 1
-        flux_diet = 12
-        flux_bone = 0.01
-        logger.info('Using flux_diet = %s and flux_bone = %s', flux_diet,
-                    flux_bone)
+    def set_flux(self, flux_DP, flux_PB):
+        """Change value"""
+        logger.info('Using flux_DP = %s and flux_PB = %s', flux_DP,
+                    flux_PB)
         self.Flux = {
-            "diet": {"diet": 0.0, "plasma": k * flux_diet, "RBC": 0e0,
-                     "liver": 0e0, "urine": 0e0, "feces": (1 - k) * flux_diet,
+            "diet": {"diet": 0.0, "plasma": flux_DP, "RBC": 0e0,
+                     "liver": 0e0, "urine": 0e0, "feces": flux_DP,
                      "muscle": 0e0, "bone": 0e0, "skin": 0e0, "kidney": 0e0},
-            "plasma": {"diet": 0.0, "plasma": 0e0, "RBC": t * 0.18e0,
-                       "liver": t * 2.64e0, "urine": 0e0,
-                       "feces": 0.75 * k * flux_diet, "muscle": t * 0.0035e0,
-                       "bone": flux_bone, "skin": 0.125 * k * flux_diet,
-                       "kidney": 0.625 * k * flux_diet},
-            "RBC": {"diet": 0.0, "plasma": t * 0.18e0, "RBC": 0e0,
+            "plasma": {"diet": 0.0, "plasma": 0e0, "RBC": 0.18e0,
+                       "liver": 2.64e0, "urine": 0e0,
+                       "feces": 0.75 * flux_DP, "muscle": 0.0035e0,
+                       "bone": flux_PB, "skin": 0.125 * flux_DP,
+                       "kidney": 0.625 * flux_DP},
+            "RBC": {"diet": 0.0, "plasma": 0.18e0, "RBC": 0e0,
                     "liver": 0.0e0, "urine": 0e0, "feces": 0e0, "muscle": 0e0,
                     "bone": 0e0, "skin": 0e0, "kidney": 0e0},
-            "liver": {"diet": 0.0, "plasma": t * 2.64e0, "RBC": 0e0,
+            "liver": {"diet": 0.0, "plasma": 2.64e0, "RBC": 0e0,
                       "liver": 0e0, "urine": 0e0, "feces": 0e0, "muscle": 0e0,
                       "bone": 0e0, "skin": 0e0, "kidney": 0e0},
             "urine": {"diet": 0.0, "plasma": 0e0, "RBC": 0e0, "liver": 0e0,
@@ -67,25 +70,25 @@ class ZnCompoIsoDiet(IsotopicBoxModel):
             "feces": {"diet": 0.0, "plasma": 0e0, "RBC": 0e0, "liver": 0e0,
                       "urine": 0e0, "feces": 0e0, "muscle": 0e0, "bone": 0e0,
                       "skin": 0e0, "kidney": 0e0},
-            "muscle": {"diet": 0.0, "plasma": t * 0.0035e0, "RBC": 0e0,
+            "muscle": {"diet": 0.0, "plasma": 0.0035e0, "RBC": 0e0,
                        "liver": 0e0, "urine": 0e0, "feces": 0e0, "muscle": 0e0,
                        "bone": 0e0, "skin": 0e0, "kidney": 0e0},
-            "bone": {"diet": 0.0, "plasma": flux_bone, "RBC": 0e0,
+            "bone": {"diet": 0.0, "plasma": flux_PB, "RBC": 0e0,
                      "liver": 0e0, "urine": 0e0, "feces": 0e0, "muscle": 0e0,
                      "bone": 0e0, "skin": 0e0, "kidney": 0e0},
             "skin": {"diet": 0.0, "plasma": 0e0, "RBC": 0e0, "liver": 0e0,
                      "urine": 0e0, "feces": 0e0, "muscle": 0e0, "bone": 0e0,
                      "skin": 0e0, "kidney": 0e0},
-            "kidney": {"diet": 0.0, "plasma": 0.5 * k * flux_diet, "RBC": 0e0,
-                       "liver": 0e0, "urine": 0.125 * k * flux_diet,
+            "kidney": {"diet": 0.0, "plasma": 0.5 * flux_DP, "RBC": 0e0,
+                       "liver": 0e0, "urine": 0.125 * flux_DP,
                        "feces": 0e0, "muscle": 0e0, "bone": 0e0, "skin": 0e0,
                        "kidney": 0e0}}
 
-    def parameters(self):
-        """ Define the time parameters, the isotopic standard and the boxes, 
+    def set_boxes(self, delta_diet):
+        """ Define the time parameters, the isotopic standard and the boxes,
         flux and partition coefficients """
         n_timestep = 100000
-        self.time = linspace(0, 18250.0, n_timestep) # temps
+        self.time = linspace(0, 18250.0, n_timestep)
 
         # JMC standard
         self.standard = 0.565203
@@ -103,16 +106,16 @@ class ZnCompoIsoDiet(IsotopicBoxModel):
             "kidney": {'Delta': 0e0, 'Mass': 2e1}
         }
 
-        #coeff_KU=1/0.9993e0;
-        coeff_KU = 1 / 0.9998e0;
+    def set_partcoeff(self, coeff_DP):
+        """Change the value of the Partition Coefficient between Diet
+        and Plasma"""
+        coeff_KU = 1 / 0.9998e0
         coeff_PRBC = 1.00025e0
-        coeff_PS = 1.000275;
-        coeff_PM = 0.99986;
-        coeff_PB = 1.0003;
-        coeff_PL = 0.99939;
-        coeff_DP=1.00018,
-        #coeff_PD=1.00025;
-        coeff_PD = 1.000;
+        coeff_PS = 1.000275
+        coeff_PM = 0.99986
+        coeff_PB = 1.0003
+        coeff_PL = 0.99939
+        coeff_PD = 1.000
 
         self.Partcoeff = {
             "diet": {"diet": 1.0, "plasma": coeff_DP, "RBC": 1e0, "liver": 1e0,
@@ -120,18 +123,36 @@ class ZnCompoIsoDiet(IsotopicBoxModel):
                      "skin": 1e0, "kidney": 1e0},
             "plasma": {"diet": 1.0, "plasma": 1e0, "RBC": coeff_PRBC,
                        "liver": coeff_PL, "urine": 1e0, "feces": coeff_PD,
-                       "muscle": coeff_PM, "bone":coeff_PB, "skin": coeff_PS, "kidney":1/coeff_KU},
-            "RBC": {"diet": 1.0, "plasma": 1e0, "RBC": 1e0, "liver": 1/coeff_PRBC, "urine": 1e0, "feces": 1e0, "muscle": 1e0, "bone":1e0, "skin": 1e0, "kidney":1e0},
-            "liver": {"diet": 1.0, "plasma": 1/coeff_PL, "RBC": 1e0, "liver": 1e0, "urine": 1e0, "feces": 1e0, "muscle": 1e0, "bone":1e0, "skin": 1e0, "kidney":1e0},
-            "urine": {"diet": 1.0, "plasma": 1e0, "RBC": 1e0, "liver": 1e0, "urine": 1e0, "feces": 1e0, "muscle": 1e0, "bone":1e0, "skin": 1e0, "kidney":1e0},
-            "feces": {"diet": 1.0, "plasma": 1e0, "RBC": 1e0, "liver": 1e0, "urine": 1e0, "feces": 1e0, "muscle": 1e0, "bone":1e0, "skin": 1e0, "kidney":1e0},
-            "muscle": {"diet": 1.0, "plasma": 1/coeff_PM, "RBC": 1e0, "liver": 1e0, "urine": 1e0, "feces": 1e0, "muscle": 1e0, "bone":1e0, "skin": 1e0, "kidney":1e0},
-            "bone": {"diet": 1.0, "plasma": 1/coeff_PB, "RBC": 1e0, "liver": 1e0, "urine": 1e0, "feces": 1e0, "muscle": 1e0, "bone":1e0, "skin": 1e0, "kidney":1e0},
-            "skin": {"diet": 1.0, "plasma": 1e0, "RBC": 1e0, "liver": 1e0, "urine": 1e0, "feces": 1e0, "muscle": 1e0, "bone":1e0, "skin": 1e0, "kidney":1e0},
-            "kidney": {"diet": 1.0, "plasma": coeff_KU, "RBC": 1e0, "liver": 1e0, "urine": coeff_KU, "feces": 1e0, "muscle": 1e0, "bone":1e0, "skin": 1e0, "kidney":1e0}
+                       "muscle": coeff_PM, "bone": coeff_PB, "skin": coeff_PS,
+                       "kidney": 1 / coeff_KU},
+            "RBC": {"diet": 1.0, "plasma": 1e0, "RBC": 1e0,
+                    "liver": 1 / coeff_PRBC, "urine": 1e0, "feces": 1e0,
+                    "muscle": 1e0, "bone": 1e0, "skin": 1e0, "kidney": 1e0},
+            "liver": {"diet": 1.0, "plasma": 1 / coeff_PL, "RBC": 1e0,
+                      "liver": 1e0, "urine": 1e0, "feces": 1e0, "muscle": 1e0,
+                      "bone": 1e0, "skin": 1e0, "kidney": 1e0},
+            "urine": {"diet": 1.0, "plasma": 1e0, "RBC": 1e0, "liver": 1e0,
+                      "urine": 1e0, "feces": 1e0, "muscle": 1e0, "bone": 1e0,
+                      "skin": 1e0, "kidney": 1e0},
+            "feces": {"diet": 1.0, "plasma": 1e0, "RBC": 1e0, "liver": 1e0,
+                      "urine": 1e0, "feces": 1e0, "muscle": 1e0, "bone": 1e0,
+                      "skin": 1e0, "kidney": 1e0},
+            "muscle": {"diet": 1.0, "plasma": 1 / coeff_PM, "RBC": 1e0,
+                       "liver": 1e0, "urine": 1e0, "feces": 1e0, "muscle": 1e0,
+                       "bone": 1e0, "skin": 1e0, "kidney": 1e0},
+            "bone": {"diet": 1.0, "plasma": 1 / coeff_PB, "RBC": 1e0,
+                     "liver": 1e0, "urine": 1e0, "feces": 1e0, "muscle": 1e0,
+                     "bone": 1e0, "skin": 1e0, "kidney": 1e0},
+            "skin": {"diet": 1.0, "plasma": 1e0, "RBC": 1e0, "liver": 1e0,
+                     "urine": 1e0, "feces": 1e0, "muscle": 1e0, "bone": 1e0,
+                     "skin": 1e0, "kidney": 1e0},
+            "kidney": {"diet": 1.0, "plasma": coeff_KU, "RBC": 1e0,
+                       "liver": 1e0, "urine": coeff_KU, "feces": 1e0,
+                       "muscle": 1e0, "bone": 1e0, "skin": 1e0, "kidney": 1e0}
             }
 
 
 if __name__ == "__main__":
     engine = ZnCompoIsoDiet()
     engine.start()
+    
